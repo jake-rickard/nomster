6360681c1409742ba381eaf501a90e3b1348fbb5
Jake Rickard <jakelrickard@gmail.com>
2018-12-22 23:56:34 +0000

    Revert "picture uploader"
    This reverts commit 73077e952453a19a50309707aae860418db6e956.
    
    reverted commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_191318) do
+ActiveRecord::Schema.define(version: 2018_12_20_015430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2018_12_22_191318) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
