9d1ad56a78b237219d763361ced5e15c789c5ef1
Jake Rickard <jakelrickard@gmail.com>
2018-12-28 02:30:19 +0000

    added pictures to photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_191318) do
+ActiveRecord::Schema.define(version: 2018_12_28_022045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
